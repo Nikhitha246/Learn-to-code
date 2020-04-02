f7dc3cab13e7eef94e5f28b2e7536c44278104a8
Nikhitha246 <62941316+Nikhitha246@users.noreply.github.com>
2020-04-02 17:46:00 +0530

    Create Average.c

---
+++ Average.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<math.h>
+
+//Compiler version gcc  6.3.0
+/*a and b are marks of the student */
+  /*Average of those marks are stored in avg*/
+int main()
+{
+  int name[20];
+  float a,b,avg;   
+  printf("Enter the name of the student: \n");
+  scanf("%s",&name[20]);
+  printf("enter the marks of the student: \n");
+  scanf("%f%f",&a,&b);
+  avg=(a+b)/2;
+  printf("The average of the marks obtained by this student is %f",avg);
+  return 0;
+}
