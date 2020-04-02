1ad10306839bcb7fdd36f772502653c2998bed62
Nikhitha246 <62941316+Nikhitha246@users.noreply.github.com>
2020-04-02 17:46:33 +0530

    Create Interest.c

---
+++ Interest.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<math.h>
+//Compiler version gcc  6.3.0
+/* p is the principal amount*/
+  /*r is the interest rate*/
+    /*t is the time period*/
+     /*i is the simple interest*/
+int main()
+{
+  float p,r,t,i;
+  printf("Enter the amount, interest rate,time period ");
+  scanf("%f%f%f",&p,&r,&t);
+  i=p*r*t;
+  printf("interest is %f",i);
+  return 0;
+}
