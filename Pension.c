0195fc45da13db8970be32276b3ea228c703a37c
Nikhitha246 <62941316+Nikhitha246@users.noreply.github.com>
2020-04-04 22:51:03 +0530

    Create Pension.c

---
+++ Pension.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+//Compiler version gcc  6.3.0
+
+int main()
+{
+  int age;
+  printf("Enter the age: ");
+  scanf("%d",&age);
+  if(age>=60)
+  printf("The amount is 10,000");
+  if(age>=50)
+  printf("The amount is 5000");
+  else
+  printf("There is no pension");
+  return 0;
+}
