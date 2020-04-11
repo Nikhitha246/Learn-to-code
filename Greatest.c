b8ed3f025e1ef9707b0fb93518c7289a11977560
Nikhitha246 <62941316+Nikhitha246@users.noreply.github.com>
2020-04-11 21:33:50 +0530

    Update Greatest.c

--- Greatest.c
+++ Greatest.c
@@ -7,9 +7,9 @@ int main()
   int a,b,c;
   printf("Enter the numbers");
   scanf("%d%d%d",&a,&b,&c);
-  if((a>b)&(c<b))
+  if((a>b)&&(c<b))
   printf("%dis the greatest",a);
-  if((b>a)&(a>c))
+  if((b>a)&&(a>c))
   printf("%d is the biggest",b);
   else
   printf("%d is the biggest",c);
